b845e3121251d479031811e238f55dcefe00b3fd
Spencer Cheung <spencercheung1@gmail.com>
2017-09-08 15:51:58 -0700

    modal added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,53 +1,115 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './style.css';
-import { Nav, NavItem } from 'react-bootstrap';
+import { Nav, NavItem, Modal, Button, MenuItem, DropdownButton } from 'react-bootstrap';
 
-import Modal from '../Modal/index';
+// import Modal from '../Modal/index';
 
 
 class Header extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { isOpen: false };
-  };
+    this.state = {
+      showDateModal: false,
+      showAddressModal: false
+    };
+  }
 
-  toggleModal = () => {
-    console.log('modaleee')
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  };
+  closeDate = () => {
+    this.setState({ showDateModal: false });
+  }
 
-  // const NavJustified = React.createClass({
-  // handleSelect(selectedKey) {
-  //   alert('selected ' + selectedKey);
-  // }
-  testfunction(){
-    console.log('jello')
+  openDate = () => {
+    this.setState({ showDateModal: true });
+  }
+
+  closeAddress = () => {
+    this.setState({ showAddressModal: false });
+  }
+
+  openAddress = () => {
+    this.setState({ showAddressModal: true });
   }
 
   render() {
   return (
-    // <div className="navbar-header">
-    // <div className="header">
-    //   <Nav bsStyle="tabs" justified activeKey={1} onSelect={this.toggleModal}>
-    //     <NavItem eventKey={1} title="Order">Order For</NavItem>
-    //     <NavItem eventKey={2} title="Date">Date & Time</NavItem>
-    //     <NavItem eventKey={3} title="Address">Enter Your Address</NavItem>
-    //   </Nav>
-    <div className="App">
-       <button onClick={this.toggleModal}>
-         Open the modal
-       </button>
+    <div className="header">
+      <Nav bsStyle="tabs" justified>
+        <NavItem eventKey={1} title="Order" onClick={this.openDate} >Order For</NavItem>
+        <NavItem eventKey={2} title="Date">Date & Time</NavItem>
+        <NavItem eventKey={3} title="Address" onClick={this.openAddress}>Enter Your Address</NavItem>
+      </Nav>
 
-       <Modal show={this.state.isOpen}
-         onClose={this.toggleModal}>
-         Here's some content for the modal
+      <div>
+       <Modal show={this.state.showDateModal} onHide={this.closeDate}>
+         <Modal.Header closeButton>
+           <Modal.Title className="text-center">When to order?</Modal.Title>
+         </Modal.Header>
+         <Modal.Body>
+           <div className="buttons-group">
+           <Button
+             className="delivery-button"
+             bsStyle="default"
+             bsSize="large">
+             DELIVERY
+           </Button>
+           <Button
+             className="pickup-button"
+             bsStyle="danger"
+             bsSize="large">
+             PICKUP
+           </Button>
+         </div>
+         <div className="pickup-dropdown">
+           <DropdownButton className="date-dropdown" title="Order For Today" bsSize="large">
+             <MenuItem eventKey="1">Today</MenuItem>
+             <MenuItem eventKey="2">Tuesday, Sept 12th</MenuItem>
+             <MenuItem eventKey="3">Wednesday, Sept 13th</MenuItem>
+             <MenuItem eventKey="4">Thursday, Sept 14th</MenuItem>
+             <MenuItem eventKey="5">Friday, Sept 15th</MenuItem>
+             <MenuItem eventKey="6">Saturday, Sept 16th</MenuItem>
+             <MenuItem eventKey="7">Sunday, Sept 17th</MenuItem>
+           </DropdownButton>
+
+           <DropdownButton className="time-dropdown" title="ASAP" bsSize="large">
+             <MenuItem eventKey="1">ASAP</MenuItem>
+             <MenuItem eventKey="2">3:00pm</MenuItem>
+             <MenuItem eventKey="3">3:30pm</MenuItem>
+             <MenuItem eventKey="4">4:00pm</MenuItem>
+             <MenuItem eventKey="5">4:30pm</MenuItem>
+           </DropdownButton>
+
+         </div>
+         </Modal.Body>
+         <Modal.Footer>
+           <div className="footer-buttons">
+           <Button className="update-button" bsStyle="default" bsSize="large">Update Results</Button>
+           <br />
+           <Button bsSize="small" onClick={this.closeDate}>Close</Button>
+         </div>
+         </Modal.Footer>
        </Modal>
      </div>
-    // </div>
+
+     <div>
+      <Modal show={this.state.showAddressModal} onHide={this.closeAddress}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>ADDRESSSSSS</h4>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+          <h4>Popover in a modal</h4>
+          <hr />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.closeAddress}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+
+    </div>
     )
   }
 }
